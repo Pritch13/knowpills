c0d069e96675f3c8360a6914944141191d42df13
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-14 11:34:25 -0600

    comment textarea

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -19,8 +19,39 @@ $(function() {
   
 
   $.get("/api/posts/" + id, function(data) {
+
     console.log(data);
+
+    var username = data[0].username;
+    var title = data[0].title;
+    var body = data[0].body;
+    var entry = "<div class='fullpost'><div class='row'><div class='col l6'><h4>"+title+"</h4></div></div><div class='row'><div class='col m12'><p>"+body+"</p></div></div><div class='row'><div class='col m12'><p class='right-align'>Created by "+username+"</p></div></div></div>";
+    $(".comment-container").append(entry);
     
 
+  });
+
+
+  $("#submitcomment").on("click", function(){
+    var commentName = $("#name").val().trim();
+    var commentBody = $("#message").val().trim();
+
+    console.log(commentName);
+    console.log(commentBody);
+
+    var newComment = {
+      username: commentName,
+      body: commentBody
+    }
+
+    if (!commentName || !commentBody) {
+      alert('Info Required!');
+  }  else {
+      $.post("/api/comment/", newComment, function() {
+          window.location.href = "/comment";
+        });
+  }
+    
+    
   });
   
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,6 +10,13 @@ module.exports = function (app) {
             });
     });
 
+    app.get("/api/comment/", function (req, res) {
+        db.Comment.findAll({})
+            .then(function (dbPost) {
+                res.json(dbPost);
+            });
+    });
+
     app.get("/api/alt/", function (req, res) {
         db.Alternative.findAll({})
             .then(function (data) {
@@ -30,6 +37,18 @@ module.exports = function (app) {
                 res.json(dbPost);
             });
     });
+
+    app.post("/api/comment", function (req, res) {
+        console.log(req.body);
+        db.Comment.create({
+            username: req.body.username,
+            body: req.body.body
+        })
+            .then(function (dbPost) {
+                res.json(dbPost);
+            });
+    });
+
     app.get("/api/posts/:id", function (req, res) {
         console.log(req.params.id);
         db.Post.findAll({
